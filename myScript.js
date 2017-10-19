b888eb8b3c13d6b4aeaf62965e6e56ddca5491e6
Erica Rao <ericarao.x@gmail.com>
2017-10-19 02:38:49 -0400

    Added jquery file, logs JSON from instagram.com/[user]/media/

--- myScript.js
+++ myScript.js
@@ -4,6 +4,13 @@ chrome.runtime.sendMessage({ifInstagram : true}, function(response) {
         var regex = new RegExp(/(instagram.com\/)([a-z0-9_-])/);
         if (regex.test(window.location.href)){
             alert("regex works, can put logic for recognizing an instagram user here");
+            $.getJSON(window.location.href + "media/", function(data) {
+                //data is the JSON string
+                console.log("getJSON working. JSON Data: ");
+                //Check console, you'll see the data from the JSON on the instagram.com/[user]/media/ show up
+                console.log(data);
+            });
+
         }
         alert("you are on an instagram page when you refresh the page");
     }
