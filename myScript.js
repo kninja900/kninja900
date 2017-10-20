b35d1f7b537a008c1b33859be642de73d43738d7
Matthew Sevey <mjsevey@gmail.com>
2017-10-19 22:01:15 -0400

    pulled our getJSON for media to reuseable function

--- myScript.js
+++ myScript.js
@@ -4,17 +4,24 @@ chrome.runtime.sendMessage({ifInstagram : true}, function(response) {
         var regex = new RegExp(exp); //instagram.com/[user]/
         if (regex.test(window.location.href)){
             alert("regex works, can put logic for recognizing an instagram user here");
-            $.getJSON(window.location.href + "media/", function(data) {
-                //data is the JSON string
-                console.log("getJSON working. JSON Data: ");
-                //Check console, you'll see the data from the JSON on the instagram.com/[user]/media/ show up
-                console.log(data);
-            });
-
+            // calling mediaJSON function to console.log JSON element
+            mediaJSON();
         }
         alert("you are on an instagram page when you refresh the page");
     }
 });
+
+// Pulling getJSON for /media out so it can be reused
+function mediaJSON() {
+  $.getJSON(window.location.href + "media/", function(data) {
+      //data is the JSON string
+      console.log("getJSON working. JSON Data: ");
+      //Check console, you'll see the data from the JSON on the instagram.com/[user]/media/ show up
+      console.log(data);
+  });
+}
+
+
 
 // Scraping Email from Bio
 // Need to replace static text with Bio from instagram API
