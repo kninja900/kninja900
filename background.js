a14d496d32c2951aab3239f9cdd6e95b878e7a98
Matthew Sevey <mjsevey@gmail.com>
2017-10-23 18:38:08 -0400

    removed instagram api code

--- background.js
+++ background.js
@@ -1,22 +1,4 @@
 chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
     // alert('The extension loaded.');
 
-    // Generates access token in new tabs
-    var client_ID = '99c741452a6e4d84a1320831f3db56e1';
-    // change redirect to mavrck to avoid loop
-    var redirect_uri = 'http://www.mavrck.co/';
-    var scope = 'public_content'
-    // var url = 'https://api.instagram.com/oauth/authorize/?client_id='+ client_ID +
-    //     '&redirect_uri='+ redirect_uri +'&response_type=token';
-    var url = 'https://api.instagram.com/oauth/authorize/?client_id='+ client_ID +
-        '&redirect_uri='+ redirect_uri +'&response_type=token&scope='+ scope;
-
-    chrome.tabs.create({ "url": url, "index": 0 }, function(tab) {
-
-      // this is evaluating to false
-      if (tab.status == "complete") {
-        alert(tab.url);
-
-      }
-    });
 });
