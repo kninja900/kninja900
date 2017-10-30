0dc7fac8d8e4ffdd038ce9cb4ae6c6c6f9c74e31
Matthew Sevey <mjsevey@gmail.com>
2017-10-30 17:35:01 -0400

    removed updatePopup function from myscript.js

--- myScript.js
+++ myScript.js
@@ -140,17 +140,6 @@
 
   }
 
-// Updating the popup.html
-// currently getting errors, getElementById is returning null
-  function updatePopup() {
-    var el = document.getElementById('engRate');
-    // if (el) {
-    //   alert("found");
-    // } else {
-    //   alert("not found");
-    // }
-  }
-
 // checks if the user ison instagram.com
   function onInstagram() {
     if(window.location.href.indexOf("instagram.com") > -1) {
